2b0f56f1deb0453b479dd97a3390c9b61a48aa9d
Sawsan Henedy <sawsanhenedy92@gmail.com>
2023-11-12 21:23:30 +0000

    h

---
+++ tests/models/test_base_model_dict.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
+
+guillaume@ubuntu:~/AirBnB$ ./test_base_model_dict.py
+56d43177-cc5f-4d6c-a0c1-e167f8c27337
+[BaseModel] (56d43177-cc5f-4d6c-a0c1-e167f8c27337) {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52298), 'my_number': 89, 'updated_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52302), 'name': 'My_First_Model'}
+<class 'datetime.datetime'>
+--
+{'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': '2017-09-28T21:03:54.052298', '__class__': 'BaseModel', 'my_number': 89, 'updated_at': '2017-09-28T21:03:54.052302', 'name': 'My_First_Model'}
+JSON of my_model:
+    id: (<class 'str'>) - 56d43177-cc5f-4d6c-a0c1-e167f8c27337
+    created_at: (<class 'str'>) - 2017-09-28T21:03:54.052298
+    __class__: (<class 'str'>) - BaseModel
+    my_number: (<class 'int'>) - 89
+    updated_at: (<class 'str'>) - 2017-09-28T21:03:54.052302
+    name: (<class 'str'>) - My_First_Model
+--
+56d43177-cc5f-4d6c-a0c1-e167f8c27337
+[BaseModel] (56d43177-cc5f-4d6c-a0c1-e167f8c27337) {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52298), 'my_number': 89, 'updated_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52302), 'name': 'My_First_Model'}
+<class 'datetime.datetime'>
+--
+False
